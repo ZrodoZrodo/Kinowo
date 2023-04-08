8fa51ce883636cd60c7eb49eb18811db7823b7b4
ZrodoZrodo <112622013+ZrodoZrodo@users.noreply.github.com>
2023-04-08 11:23:08 +0200

    Update user.service.ts

--- nest-kinowo-backend/src/user/user.service.ts
+++ nest-kinowo-backend/src/user/user.service.ts
@@ -1,124 +1,113 @@
 import { Injectable } from '@nestjs/common';
-import prisma from "../../prisma/prisma";
-import {CreateUserDto} from "../../dto/create-user.dto";
+import prisma from '../../prisma/prisma';
+import { CreateUserDto } from '../../dto/create-user.dto';
 import * as bcrypt from 'bcrypt';
-import {LoginUserDto} from "../../dto/loginUser.dto";
+import { LoginUserDto } from '../../dto/loginUser.dto';
 
 @Injectable()
 export class UserService {
-
-
-    async register({name,lastName,email,password}:CreateUserDto):Promise<{status:number,message:string}>{
-        try {
-            password=await bcrypt.hash(password,process.env.SALT)
-            console.log(password)
-             await prisma.user.create({
-                data: {
-                    email,
-                    name,
-                    password,
-                    lastName,
-                }
-
-            })
-            return {status:201,message:"Created"}
-        }catch(e)
-        {
-            return e.code=='P2002'?{status:409,message:"This email already exist"}:e.message
-        }
-        }
-
-       async login({email,password}:LoginUserDto){
-        try {
-            password = await bcrypt.hash(password, process.env.SALT)
-            return await prisma.user.findFirst({
-                where: {email, password},
-                select: {
-                    id: true,
-                    name: true,
-                    email: true,
-                }
-            })
-        }
-        catch (e)
-        {
-            console.log(e.code+" "+e.message)
-        }
-       }
-       async getUser(id:string){
-        return await prisma.user.findFirst({
-            where:{id},
-            select:{
-                id:true,
-                email:true,
-                name:true,
-                lastName:true,
-            }
-        })
-       }
-       async getUserActiveReservations(id:string)
-       {
-           return await prisma.user.findFirst({
-               where:{id},
-               select:{
-                    reservations:{
-                        where:{watched:true},
-                    }
-               }
-           })
-       }
-
-    async getUserComingReservations(id:string)
-    {
-        return await prisma.user.findFirst({
-            where:{id},
-            select:{
-                reservations:{
-                    where:{watched:false},
-                }
-            }
-        })
+  async register({
+    name,
+    lastName,
+    email,
+    password,
+  }: CreateUserDto): Promise<{ status: number; message: string }> {
+    try {
+      password = await bcrypt.hash(password, process.env.SALT);
+      console.log(password);
+      await prisma.user.create({
+        data: {
+          email,
+          name,
+          password,
+          lastName,
+        },
+      });
+      return { status: 201, message: 'Created' };
+    } catch (e) {
+      return e.code == 'P2002'
+        ? { status: 409, message: 'This email already exist' }
+        : e.message;
     }
+  }
 
-       async createReservation(
-           userId:string,
-           movieId:string,
-           title:string,
-           date:Date,
-           price:number,
-           screeningNumber:number,
-           seatNumber :number
-       ){
-        return await prisma.reservation.create({
-            data:{
-                movieId,
-                title,
-                date,
-                price,
-                screeningNumber,
-                seatNumber,
-                user:{
-                    connect:{
-                        id:userId
-                    }
-                }
-            }
-        })
+  async login({ email, password }: LoginUserDto) {
+    try {
+      password = await bcrypt.hash(password, process.env.SALT);
+      return await prisma.user.findFirst({
+        where: { email, password },
+        select: {
+          id: true,
+          name: true,
+          email: true,
+        },
+      });
+    } catch (e) {
+      console.log(e.code + ' ' + e.message);
     }
-    async updateUser(
-        email:string,
-        name:string,
-        lastName:string,
-        id:string)
-    {
-        return prisma.user.update({
-            where:{id},
-        data:{email,
-            name,
-            lastName,
-        }
+  }
+  async getUser(id: string) {
+    return await prisma.user.findFirst({
+      where: { id },
+      select: {
+        id: true,
+        email: true,
+        name: true,
+        lastName: true,
+      },
+    });
+  }
+  async getUserActiveReservations(id: string) {
+    return await prisma.user.findFirst({
+      where: { id },
+      select: {
+        reservations: {
+          where: { watched: true },
+        },
+      },
+    });
+  }
 
-        })
-    }
+  async getUserComingReservations(id: string) {
+    return await prisma.user.findFirst({
+      where: { id },
+      select: {
+        reservations: {
+          where: { watched: false },
+        },
+      },
+    });
+  }
 
+  async createReservation(
+    userId: string,
+    movieId: string,
+    title: string,
+    date: Date,
+    price: number,
+    screeningNumber: number,
+    seatNumber: number,
+  ) {
+    return await prisma.reservation.create({
+      data: {
+        movieId,
+        title,
+        date,
+        price,
+        screeningNumber,
+        seatNumber,
+        user: {
+          connect: {
+            id: userId,
+          },
+        },
+      },
+    });
+  }
+  async updateUser(email: string, name: string, lastName: string, id: string) {
+    return prisma.user.update({
+      where: { id },
+      data: { email, name, lastName },
+    });
+  }
 }
